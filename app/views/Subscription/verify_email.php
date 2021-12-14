7be15cfa5742b4f7984013957ece12c1b9cb85ab
Vinverre <96021901+Vinverre@users.noreply.github.com>
2021-12-14 11:39:06 +0100

    Add files via upload

---
+++ app/views/Subscription/verify_email.php
@@ -0,0 +1,20 @@
+<?php
+$verifyMessage = '';
+if(!empty($_GET['email_verify'])){     
+	$token = $_GET['email_verify']; 	
+	$subscriber->verify_token = $token;
+    if($subscriber->isValidToken()){ 
+       	$subscriber->is_verified = 1;        
+        if($subscriber->update()) { 
+            $verifyMessage = '<p class="success">Congratulations. Your email 
+			address has been verified successfully.</p>'; 
+        } else { 
+            $verifyMessage = '<p class="error">Some problem 
+			occurred on verifying your email, please try again later.</p>'; 
+        } 
+    } else { 
+        $verifyMessage = '<p class="error">You have clicked on 
+		the wrong link, please check your email and try again.</p>'; 
+	}
+}
+
